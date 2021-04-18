248119658a8cdbcacc9871737a5fc96af9cf556a
Jonathas Veras <Jonathasveras42@gmail.com>
2021-04-18 15:34:09 -0300

    Create 1050 - DDD.cpp

---
+++ 1050 - DDD.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define MAX_STRING 100;
+struct Cidade{
+    int ddd;
+    char destination[MAX_STRING];
+};
+int main()
+{
+    char nomes_cidades[8] = ["Brasilia", "Salvador", "Sao Paulo", "Rio de Janeiro", "Juiz de Fora", "Campinas", "Vitoria", "Belo Horizonte"];
+    Cidade cidades[8];
+    
+    return 0;
+}
