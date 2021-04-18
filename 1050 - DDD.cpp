5c879201fdfa721386c9017de5f7d4db65db6271
Jonathas Veras <Jonathasveras42@gmail.com>
2021-04-18 19:59:31 -0300

    Update 1050 - DDD.cpp

--- 1050 - DDD.cpp
+++ 1050 - DDD.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
-#define MAX_STRING 100;
-struct Cidade{
-    int ddd;
-    char destination[MAX_STRING];
+#define MAX_STRING 100
 };
 int main()
 {
-    char nomes_cidades[8] = ["Brasilia", "Salvador", "Sao Paulo", "Rio de Janeiro", "Juiz de Fora", "Campinas", "Vitoria", "Belo Horizonte"];
-    Cidade cidades[8];
+    char nomes_cidades = {"Brasilia", "Salvador", "Sao Paulo", "Rio de Janeiro", "Juiz de Fora", "Campinas", "Vitoria", "Belo Horizonte"};
+    const int ddd[8] = {61, 71, 11, 21, 32, 19, 27, 31}
     
     return 0;
 }
