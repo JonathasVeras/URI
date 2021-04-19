e62f188dcd9f6f6d32524a74bb843c70caaf487b
Jonathas Veras <Jonathasveras42@gmail.com>
2021-04-19 17:49:13 -0300

    Update 1050 - DDD.cpp

--- 1050 - DDD.cpp
+++ 1050 - DDD.cpp
@@ -4,7 +4,7 @@ using namespace std;
 };
 int main()
 {
-    char nomes_cidades = {"Brasilia", "Salvador", "Sao Paulo", "Rio de Janeiro", "Juiz de Fora", "Campinas", "Vitoria", "Belo Horizonte"};
+    const char nomes_cidades = {"Brasilia", "Salvador", "Sao Paulo", "Rio de Janeiro", "Juiz de Fora", "Campinas", "Vitoria", "Belo Horizonte"};
     const int ddd[8] = {61, 71, 11, 21, 32, 19, 27, 31}
     
     return 0;
