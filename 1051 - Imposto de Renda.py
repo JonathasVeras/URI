15766925ea20de901d1042f2de95c9f2913a0e9c
Jonathas Veras <Jonathasveras42@gmail.com>
2021-04-24 16:30:48 -0300

    Update 1051 - Imposto de Renda.py

--- 1051 - Imposto de Renda.py
+++ 1051 - Imposto de Renda.py
@@ -1,15 +1,21 @@
 def imposto(salario_cliente):
-    salario_cliente = float(salario_cliente)
-    if salario_cliente >= 4500:
-        return (salario_cliente - 4500 * 0.28) + imposto(salario_cliente - (salario_cliente-4500))
-    elif 3000.01 <= salario_cliente <= 4500:
-        return (salario_cliente - 3000 * 0.18) + imposto(salario_cliente - (salario_cliente - 3000))
-    elif 2000.01 <= salario_cliente <= 3000:
-        return (salario_cliente - 2000 * 0.08) + imposto(salario_cliente - (salario_cliente - 2000))
-    elif salario_cliente < 2000:
+    imposto_renda = salario_cliente
+    if salario_cliente <= 2000:
         return 0
+    elif salario_cliente > 4500:
+        imposto_renda = ((salario_cliente - 4500) * 0.28)
+        return imposto_renda + imposto(salario_cliente - (salario_cliente - 4500))
+    elif 3000.01 <= salario_cliente <= 4500:
+        imposto_renda = ((salario_cliente - 3000) * 0.18)
+        return imposto_renda + imposto(salario_cliente - (salario_cliente - 3000))
+    elif 2000.01 <= salario_cliente <= 3000:
+        imposto_renda = ((salario_cliente - 2000) * 0.08)
+        return imposto_renda + imposto(salario_cliente - (salario_cliente - 2000))
 
 
-salario = input()
+salario = float(input())
 resultado = imposto(salario)
-print(resultado)
+if resultado == 0:
+    print('Isento')
+else:
+    print(f'R$ {resultado:.2f}')
