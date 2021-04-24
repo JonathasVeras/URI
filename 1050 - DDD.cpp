935c4e1c96f9a5a581e8a957e77e3a5e2e3b42b3
Jonathas Veras <Jonathasveras42@gmail.com>
2021-04-24 15:38:38 -0300

    Update 1050 - DDD.cpp

--- 1050 - DDD.cpp
+++ 1050 - DDD.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
 using namespace std;
 #define MAX_STRING 100
-};
+#define MAX 100
+
+int check_ddd(int ddd[MAX], int tamanho, int x);
+
 int main()
 {
-    const char nomes_cidades = {"Brasilia", "Salvador", "Sao Paulo", "Rio de Janeiro", "Juiz de Fora", "Campinas", "Vitoria", "Belo Horizonte"};
-    const int ddd[8] = {61, 71, 11, 21, 32, 19, 27, 31};
+    char nomes_cidades[8][MAX_STRING]= {"Brasilia", "Salvador", "Sao Paulo", "Rio de Janeiro", "Juiz de Fora", "Campinas", "Vitoria", "Belo Horizonte"};
+    int ddd[8] = {61, 71, 11, 21, 32, 19, 27, 31};
     int x;
     cin >> x;
-    for (size_t i = 0; i < 8; i++)
+    if(check_ddd(ddd, 8, x) != 8)
     {
-        if (x == i)
-        {
-                    
-        }
+      cout << nomes_cidades[check_ddd(ddd, 8, x)] << endl;
     }
-    
-    
+    else
+    {
+      cout <<"DDD nao cadastrado" << endl;
+    }
+
     return 0;
 }
 
+
+int check_ddd(int ddd[MAX], int tamanho, int x)
+{
+  for (size_t i = 0; i < tamanho; i++)
+  {
+    if(x == ddd[i])
+    {
+      return i;
+    }
+  }
+  return tamanho;
+}
