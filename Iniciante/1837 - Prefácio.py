9a603de6bd365f63b9012e499f8d5dcfac962f9b
Jonathas Veras <Jonathasveras42@gmail.com>
2021-04-30 13:38:39 -0300

    Create 1837 - Prefácio.py

---
+++ Iniciante/1837 - Prefácio.py
@@ -0,0 +1,54 @@
+def quociente(a, b, direcao=1):
+    ############################### 'a' e 'b' positivos  ###############################
+    if direcao == 1:
+        quociente_int = 1
+        resto = 0
+        while b * quociente_int < a:
+            quociente_int += 1
+            if b * quociente_int > a:
+                quociente_int -= 1
+                break
+        while b * quociente_int + resto != a:
+            resto += 1
+        print(quociente_int, resto)
+        ############################### 'a' e 'b' negativos ###############################
+    elif direcao == 2:
+        quociente_int = 1
+        resto = -1
+        while b * quociente_int > a:
+            quociente_int += 1
+            if b * quociente_int < a:
+                quociente_int -= 1
+                break
+        while b * quociente_int + resto != a:
+            resto -= 1
+        print(quociente_int, resto)
+        ############################### 'a' negativo e 'b' positivo ###############################
+    elif direcao == 3:
+        quociente_int = -1
+        resto = 0
+        while b * quociente_int > a:
+            quociente_int -= 1
+            if b * quociente_int <= a:
+                quociente_int += 1
+                break
+        while b * quociente_int + resto != a:
+            resto += -1
+        print(quociente_int, abs(resto))
+    ############################### 'a' positivo e 'b' negativo ###############################
+    elif direcao == 4:
+        quociente_int = -1
+        resto = 0
+        while b * quociente_int < a:
+            quociente_int -= 1
+            if b * quociente_int > a:
+                quociente_int += 1
+                break
+        while b * quociente_int + resto != a:
+            resto += 1
+        print(quociente_int, resto)
+
+
+a, b = map(int, input().split())
+
+quociente(a, b, direcao=4)
